356cea98469d41c2a8c74d2242d0969ed0bbbbe5
Pranav Doshi <pranav.rdoshi@gmail.com>
2026-07-21 18:50:35 -0600

    Add: buy-and-sell-crypto - submission-4

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-4.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        int i = 0;
+        int j = n - 1;
+
+        int max = 0;
+        int lowest = INT_MAX;
+        for (int i = 0; i < n; i++) {
+            int profit = prices[i] - lowest;
+            if (profit > max) max = profit;
+            if (prices[i] < lowest) lowest = prices[i];
+        }
+
+        return max;
+    }
+};
