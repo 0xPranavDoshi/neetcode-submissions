d52088a521632f76646485a582bea18f7fe3838a
Pranav Doshi <pranav.rdoshi@gmail.com>
2026-09-19 16:33:09 -0600

    Add: best-time-to-buy-and-sell-stock-ii - submission-2

---
+++ Data Structures & Algorithms/best-time-to-buy-and-sell-stock-ii/submission-2.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+
+        int totalProfit = 0;
+        int buyingPrice = prices[0];
+        int i = 1;
+        while (i < n) {
+            int sellingPrice = prices[i];
+
+            if (prices[i-1] < buyingPrice) {
+                buyingPrice = prices[i-1];
+            }
+
+            if (i != n-1 && prices[i+1] > prices[i]) {
+                i++;
+                continue;
+            }
+
+            int profit = sellingPrice - buyingPrice;
+            if (profit > 0) totalProfit += profit;
+            if (i+1 >= n) break;
+            buyingPrice = prices[i+1];            
+            i += 2;
+        }
+
+        return totalProfit;
+    }
+};
